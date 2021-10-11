d8849304fba4ab2c73b95a22abbd64b769ccd461
karnavsheth <karnavsheth@yahoo.in>
2021-10-11 09:01:04 +0100

    corrected structure and clean up

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieListing.Classes;
 using Newtonsoft.Json;
 
 namespace MovieListing.Controllers
@@ -14,43 +15,30 @@ namespace MovieListing.Controllers
     public class MoviesController : ControllerBase
     {
 
-        private List<Item> LoadJson()
+        private List<Movie> LoadJson()
         {
             StreamReader r = new StreamReader("movies.json");
                 string json = r.ReadToEnd();
                 dynamic array = JsonConvert.DeserializeObject(json);
                 var str = JsonConvert.SerializeObject(array["movies"]);
-                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(str);
+                List<Movie> items = JsonConvert.DeserializeObject<List<Movie>>(str);
             r.Close();
                 return items;
         }
 
-        public class Item
-        {
-            public string Language { get; set; }
-            public string Location { get; set; }
-            public string Plot { get; set; }
-            public string Poster { get; set; }
-            public string[] SoundEffects { get; set; }
-            public string[] Stills { get; set; }
-            public string Title { get; set; }
-            public string imdbID{ get; set; }
-        public string listingType { get; set; }
-        public string imdbRating { get; set; }
-    }
 
         [HttpGet("/api/movies/GetAll")]
-        public ActionResult<List<Item>> GetMovieList()
+        public ActionResult<List<Movie>> GetMovieList()
         {
-            var list = LoadJson();
-            return Ok(list);
+            var movieList = LoadJson();
+            return Ok(movieList);
         }
 
         [HttpGet("/api/movies/{Id}")]
-        public ActionResult<Item> GetMovieList(string Id)
+        public ActionResult<Movie> GetMovieList(string Id)
         {
-            var list = LoadJson().Where(x=>x.imdbID==Id).FirstOrDefault();
-            return Ok(list);
+            var movieDetails = LoadJson().Where(x=>x.imdbID==Id).FirstOrDefault();
+            return Ok(movieDetails);
         }
     }
 }
